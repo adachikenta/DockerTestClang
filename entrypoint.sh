528ed1bfae992c834a4d88fab95fcc2142e39425
@dachi_kenta <adachi_kenta_real@hotmail.com>
2021-03-10 21:53:49 +0900

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,22 @@
 
 echo "Hello $1"
 
-cd ./home/devenv/_build_/
-make
+ls -l
+cd ./home/
+ls -l
+cd ./devenv/
+ls -l
+cd ./_build_/
+#cd ./home/devenv/_build_/
+#make
 
-echo ^^^^ check ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-[ -f ./application ] && echo application exist || echo application not exist
-[ -f ./application ] && ./application
-echo $?
+#echo ^^^^ check ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#[ -f ./application ] && echo application exist || echo application not exist
+#[ -f ./application ] && ./application
+#echo $?
 
-echo ^^^^ clean ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-make clean
+#echo ^^^^ clean ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#make clean
 
 time=$(date)
 echo "::set-output name=time::$time"
